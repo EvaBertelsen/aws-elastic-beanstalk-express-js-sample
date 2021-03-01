d7ac821819a1479f6810cdfb86542d66270d869f
evaab <11019@student.eadania.dk>
2021-03-01 09:07:07 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('This costs a ton-fuck of money to learn'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
